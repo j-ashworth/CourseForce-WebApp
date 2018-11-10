0e50c1e2b4989f90fc7e12f6a695ae11c3cc5608
danieljakeb <daniel.bedrossian@mail.utoronto.ca>
2018-11-10 16:09:31 -0500

    Hashing and Create Account functionality

---
+++ mie350webapp/src/com/mie/util/HashText.java
@@ -0,0 +1,28 @@
+// code was taken from http://www.sha1-online.com/sha256-java/
+
+package com.mie.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+ 
+public class HashText {
+ 
+    /**
+     * @param args
+     * @throws NoSuchAlgorithmException 
+     */
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        System.out.println(sha256("test string to sha256"));
+    }
+     
+    public static String sha256(String input) throws NoSuchAlgorithmException {
+        MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+         
+        return sb.toString();
+    }
+}
