ecf624c2ed7ff99755b6965332f67cc448c55cb1
Jack Berkshire <jack.berkshire@mail.utoronto.ca>
2018-11-10 21:43:09 -0500

    gang gang i did it again

---
+++ mie350webapp/src/com/mie/dao/CourseDao.java
@@ -0,0 +1,58 @@
+package com.mie.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mie.model.Course;
+import com.mie.model.Student;
+import com.mie.util.DbUtil;
+
+
+public class CourseDao {
+
+	private Connection connection;
+	
+	public CourseDao() {
+		connection = DbUtil.getConnection();
+	}
+	
+	public List<Course> getCourseByKeyWord(String keyword) {
+		
+		List<Course> courses = new ArrayList<Courses>()
+		try {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("select * from course where "
+							+ "courseCode LIKE ? OR name LIKE ? OR description LIKE ?");
+
+			preparedStatement.setString(1, "%" + keyword + "%");
+			preparedStatement.setString(2, "%" + keyword + "%");
+			preparedStatement.setString(3, "%" + keyword + "%");
+			ResultSet rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				Course course = new Course();
+				course.setCourseCode(rs.getString("courseCode"));
+				course.setCourseLevel(rs.getInt("courseLevel"));
+				course.setCs((rs.getInt("cs"));
+				course.setDept(rs.getString("dept"));
+				course.setDescription(rs.getString("description"));
+				course.setHss(rs.getInt("hss"));
+				course.setLecHours(rs.getInt("lecHours"));
+				course.setName(rs.getString("name"));
+				course.setNs(rs.getInt("ns"));
+				course.setPraHours(rs.getInt("praHours"));
+				course.setTutHours(rs.getInt("tutHours"));
+				courses.add(course);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return courses;
+	}
+	
+}
---
+++ mie350webapp/src/com/mie/model/Course.java
@@ -0,0 +1,112 @@
+package com.mie.model;
+
+public class Course {
+	private String courseCode;
+	private String name;
+	private String dept;
+	private String description;
+	private int hss;
+	private int cs;
+	private int ns;
+	private int courseLevel;
+	private int lecHours;
+	private int tutHours;
+	private int praHours;
+	
+	public String getCourseCode() {
+		return courseCode;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDept() {
+		return dept;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public int getHss() {
+		return hss;
+	}
+	
+	public int getNs() {
+		return ns;
+	}
+	
+	public int getCs() {
+		return cs;
+	}
+	
+	public int getCourseLevel() {
+		return courseLevel;
+	}
+	
+	public int getLecHours() {
+		return lecHours;
+	}
+	
+	public int getTutHours() {
+		return tutHours;
+	}
+	
+	public int getPraHours() {
+		return praHours;
+	}
+	
+	public void setCourseCode(String cc) {
+		courseCode = cc;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	
+	public void setDescription(String d) {
+		description = d;
+	}
+	
+	public void setHss(int hss) {
+		this.hss = hss;
+	}
+	
+	public void setNs(int ns) {
+		this.ns = ns;
+	}
+	
+	public void setCs(int cs) {
+		this.cs = cs;
+	}
+	
+	public void setCourseLevel(int cl) {
+		courseLevel = cl;
+	}
+	
+	public void setLecHours(int lh) {
+		lecHours = lh;
+	}
+	
+	public void setPraHours(int ph) {
+		praHours = ph;
+	}
+	
+	public void setTutHours(int th) {
+		tutHours = th;
+	}
+	
+	@Override
+	public String toString() {
+		return "Course: [Course Code: " + courseCode + ",Name: " + name + ",Department: " + dept +
+				",Description: " + description + ",HSS: " + hss + ",CS: " + cs + ",NS: " + ns  +
+				",Course Level: " + courseLevel + ",Lecture Hours: " + lecHours + ",Practical Hours: " +
+				praHours + ",Tutorial Hours: " + tutHours "]";
+	}
+	
+}
